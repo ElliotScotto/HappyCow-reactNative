39bc3c880fa517925e1f0213a02583fed11250ae
Elliot Scotto <112620044+ElliotScotto@users.noreply.github.com>
2022-12-08 22:34:26 +0100

    Jour11FiltersColorsAndStatesOK

--- components/Filters.js
+++ components/Filters.js
@@ -8,79 +8,127 @@ import {
   Image,
   Dimensions,
 } from "react-native";
+//
+
+//
 export default function Filters({ filter, setFilter }) {
+  const [colorVegan, setColorVegan] = useState("#ffffff");
+  const [colorVegetarian, setColorVegetarian] = useState("#ffffff");
+  const [colorVegOptions, setColorVegOptions] = useState("#ffffff");
+  const [colorFilter, setColorFilter] = useState("#ffffff");
+  const [colorTextVegan, setColorTextVegan] = useState("black");
+  const [colorTextVegetarian, setColorTextVegetarian] = useState("black");
+  const [colorTextVegOptions, setColorTextVegOptions] = useState("black");
   return (
     <View style={[styles.filters, styles.shadowFilter]}>
-      <TouchableOpacity
+      <View
+        backgroundColor={colorVegan}
         style={[styles.btnFilters, styles.shadowFilter]}
-        onPress={() => {
-          if (filter === "vegan") {
-            setFilter("");
-          } else if (!filter) {
-            setFilter("vegan");
-          }
-        }}
       >
-        <View marginBottom={4}>
-          <Image
-            style={styles.iconType}
-            source={require("../assets/Icons/png/vegan.png")}
-          />
-        </View>
-        <View>
-          <Text style={styles.textFilter}>Vegan</Text>
-        </View>
-      </TouchableOpacity>
-      <TouchableOpacity
+        <TouchableOpacity
+          onPress={() => {
+            if (filter === "vegan") {
+              setFilter("");
+              setColorVegan("#ffffff");
+              setColorTextVegan("black");
+            } else if (!filter) {
+              setFilter("vegan");
+              setColorVegan("#43A047");
+              setColorTextVegan("#ffffff");
+            }
+          }}
+        >
+          <View marginBottom={4} alignItems={"center"}>
+            <Image
+              style={styles.iconType}
+              source={require("../assets/Icons/png/vegan.png")}
+            />
+          </View>
+          <View>
+            <Text style={[styles.textFilter, { color: colorTextVegan }]}>
+              Vegan
+            </Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+      <View
+        backgroundColor={colorVegetarian}
         style={[styles.btnFilters, styles.shadowFilter]}
-        onPress={() => {
-          if (filter === "vegetarian") {
-            setFilter("");
-          } else if (!filter) {
-            setFilter("vegetarian");
-          }
-        }}
       >
-        <View marginBottom={4}>
-          <Image
-            style={styles.iconType}
-            source={require("../assets/Icons/png/vegetarian.png")}
-          />
-        </View>
-        <View>
-          <Text style={styles.textFilter}>Vegetarian</Text>
-        </View>
-      </TouchableOpacity>
-      <TouchableOpacity
+        <TouchableOpacity
+          style={[styles.btnFilters, styles.shadowFilter]}
+          onPress={() => {
+            if (filter === "vegetarian") {
+              setFilter("");
+              setColorVegetarian("#ffffff");
+              setColorTextVegetarian("black");
+            } else if (!filter) {
+              setFilter("vegetarian");
+              setColorVegetarian("#9C4EA1");
+              setColorTextVegetarian("#ffffff");
+            }
+          }}
+        >
+          <View marginBottom={4} alignItems={"center"}>
+            <Image
+              style={styles.iconType}
+              source={require("../assets/Icons/png/vegetarian.png")}
+            />
+          </View>
+          <View>
+            <Text style={[styles.textFilter, { color: colorTextVegetarian }]}>
+              Vegetarian
+            </Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+      <View
+        backgroundColor={colorVegOptions}
         style={[styles.btnFilters, styles.shadowFilter]}
-        onPress={() => {
-          if (filter === "veg-options") {
-            setFilter("");
-          } else if (!filter) {
-            setFilter("veg-options");
-          }
-        }}
       >
-        <View marginBottom={4}>
-          <Image
-            style={styles.iconType}
-            source={require("../assets/Icons/png/veg-options.png")}
-          />
-        </View>
-        <View>
-          <Text numberOfLines={1} style={styles.textFilter}>
-            Veg-options
-          </Text>
-        </View>
-      </TouchableOpacity>
-      <TouchableOpacity style={[styles.btnFilters, styles.shadowFilter]}>
-        <View marginBottom={4}>
-          <Entypo name="sound-mix" size={24} color="grey" />
-        </View>
-        <View>
-          <Text style={styles.textFilter}>Filtres</Text>
-        </View>
-      </TouchableOpacity>
+        <TouchableOpacity
+          style={[styles.btnFilters, styles.shadowFilter]}
+          onPress={() => {
+            if (filter === "veg-options") {
+              setFilter("");
+              setColorVegOptions("#ffffff");
+              setColorTextVegOptions("black");
+            } else if (!filter) {
+              setFilter("veg-options");
+              setColorVegOptions("#E17878");
+              setColorTextVegOptions("#ffffff");
+            }
+          }}
+        >
+          <View marginBottom={4} alignItems={"center"}>
+            <Image
+              style={styles.iconType}
+              source={require("../assets/Icons/png/veg-options.png")}
+            />
+          </View>
+          <View>
+            <Text
+              numberOfLines={1}
+              style={[styles.textFilter, { color: colorTextVegOptions }]}
+            >
+              Veg-options
+            </Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+      <View
+        backgroundColor={colorFilter}
+        style={[styles.btnFilters, styles.shadowFilter]}
+      >
+        <TouchableOpacity style={[styles.btnFilters, styles.shadowFilter]}>
+          <View marginBottom={4} alignItems={"center"}>
+            <Entypo name="sound-mix" size={24} color="grey" />
+          </View>
+          <View>
+            <Text style={styles.textFilter}>Filtres</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -107,7 +155,6 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     width: widthScreen * 0.22,
     borderRadius: 10,
-    backgroundColor: "#ffffff",
   },
   iconType: {
     width: 24,
